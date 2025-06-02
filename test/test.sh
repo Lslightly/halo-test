aa962052c239037b08c2744ca436fa2893f80e4c
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2025-06-02 12:32:00 +0800

    fix script

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 gcc test.c -g -O0 -no-pie -falign-functions=4096 -o test
-halo baseline --jemalloc --trials 10 --pmu-events=L1-dcache-load-misses -- ./test
-halo run --jemalloc --trials 10 --pmu-events=L1-dcache-load-misses --affinity-distance 128 -- ./test -- ./test
+halo baseline --jemalloc --trials 10 --pmu-events=L1-dcache-load-misses --directory ../results/test_tmp -- ./test
+halo run --jemalloc --trials 10 --pmu-events=L1-dcache-load-misses --affinity-distance 128 --directory ../results/test_tmp -- ./test -- ./test
